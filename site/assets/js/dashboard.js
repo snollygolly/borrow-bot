951f0a531259630d4e154f1e6a75de0f6dd4dc5a
SLoW <snollygolly@gmail.com>
2015-11-15 20:00:31 -0600

    made pagination finally work right

--- site/assets/js/dashboard.js
+++ site/assets/js/dashboard.js
@@ -8,18 +8,34 @@ $( document ).ready(function() {
     // they specified which page they are on
     page = Number(page[2]);
   }
+  // check for next
   if (page <= 1){
     $(".next").toggleClass("disabled");
-    $('.next').prop("disabled", true);
+    $(".next a").attr("href", "#");
+  }else{
+    $(".next a").attr("href", "/dashboard/" + (page - 1));
   }
+  // check for previous
   if (results != 25){
     $(".previous").toggleClass("disabled");
-    $('.previous').prop("disabled", true);
+    $(".previous a").attr("href", "#");
+  }else{
+    $(".previous a").attr("href", "/dashboard/" + (page + 1));
   }
-  $(".next a").attr("href", "/dashboard/" + (page - 1));
-  $(".previous a").attr("href", "/dashboard/" + (page + 1));
 
-  $(".clickable-row").click(function() {
+  $(".next").click(function(e){
+    if ($(".next").hasClass("disabled")){
+      e.preventDefault();
+    }
+  });
+
+  $(".previous").click(function(e){
+    if ($(".previous").hasClass("disabled")){
+      e.preventDefault();
+    }
+  });
+
+  $(".clickable-row").click(function(e) {
     window.document.location = "http://reddit.com/r/borrow/" + $(this).data("post-id");
   });
 });
