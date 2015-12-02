a1a38ae753ae520a2cb12e95b325973303d56d3f
SLoW <snollygolly@gmail.com>
2015-12-01 19:43:14 -0600

    cleaning up some console logs

--- site/controllers/accounts.js
+++ site/controllers/accounts.js
@@ -7,14 +7,10 @@ const model = require('../models/accounts');
 let user = null;
 
 module.exports.index = function* index(){
-	console.log(this.isAuthenticated());
 	if (this.isAuthenticated()) {
-		console.log("setting user");
 	  user = this.session.passport.user;
 	}else{
-		console.log("redirecting");
 		return this.redirect('/');
 	}
-	console.log(user);
 	yield this.render('settings', {title: config.site.name, user: user});
 }
